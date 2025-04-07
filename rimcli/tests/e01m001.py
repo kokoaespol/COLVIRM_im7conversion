4fc510ec14f3fcb71a3df1a4564a391c81eb4c86
AnthonyyHL <anthleon@espol.edu.ec>
2025-04-07 15:20:47 -0500

    Update path handling to be cross-platform compatible

--- rimcli/tests/e01m001.py
+++ rimcli/tests/e01m001.py
@@ -3,15 +3,19 @@
 # First automated test
 # e01m001: <e>xample of testing category <01> for i<m>7 images. Test number <0001>
 
-def test(file):
-    import os
+def test(file):    
+    import pathlib
     import subprocess
-    path = os.path.dirname(file)
-    imgdat = '/B00001.im7'
-    path = path + imgdat
-    print('Testing started in ',path)
-    subprocess.run(['rim-cli --single-im7 ' + path],shell=True, executable="/bin/bash")
-    print('Testing ended in ',path)
+    base_dir = pathlib.Path(file).parent
+    imgdat = 'B00001.im7'
+    image_path = base_dir / imgdat
+    print('Testing started in ', image_path)
+    result = subprocess.run(['rim-cli', '--single-im7', str(image_path)], shell=True)
+
+    if result.returncode != 0:
+        print(f"Error executing command: {result.stderr}")
+        
+    print('Testing ended in ',image_path)
     
 if __name__=='__main__':
     from rimcli.tests.data import datlnk
