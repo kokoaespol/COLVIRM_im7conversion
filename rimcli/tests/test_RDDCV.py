f334332fc6d56f9ca196859a399928b3a02a3c72
MelissaAyllon <mayllon@espol.edu.ec>
2025-05-17 12:38:10 -0500

    ADD test for RDCLI use case

---
+++ rimcli/tests/test_RDDCV.py
@@ -0,0 +1,20 @@
+import pytest
+
+def test_RDDCV_RDCLI_should_pass_correct_number_arguments_():
+    
+    # Import the function to be tested
+    from rimcli.modcli.RDDCV import RDCLI
+
+    # Mock the command line arguments
+    import sys
+    from unittest.mock import patch
+    with patch.object(sys, 'argv', ['script_name', 'arg1', 'arg2']):
+        # Call the function
+        error, clinpt = RDCLI()
+
+        # Check that the error is False
+        assert not error
+
+        # Check that clinpt has the expected number of arguments
+        assert len(clinpt) == 3  # script_name + arg1 + arg2
+
