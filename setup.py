fbba238dad3c9b187e758597db0f1ddfef8df8ae
cxag <carlosxag1992@gmail.com>
2025-07-14 01:07:44 +0100

    Formatted v 0.0.20 ./setup.py via isort

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
-from setuptools import setup, find_packages
 import sys
+
+from setuptools import find_packages, setup
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
